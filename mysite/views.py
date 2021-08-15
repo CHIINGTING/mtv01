837ed4751b2785962a74a6fb9f944e9ed1479f40
CHIINGTING <tad20077@gmail.com>
2021-08-16 00:22:37 +0800

    add template

--- mysite/views.py
+++ mysite/views.py
@@ -1,60 +1,18 @@
-from django.http import HttpResponse
+import random
 from django.shortcuts import render
 from mysite.models import Product
 
 
 # Create your views here.
 def about(request):
-    html = '''<!DOCTYPE html>
-    <html>
-        <head>
-            <title>About Myself</title>
-        </head>
-        <body>
-            <h2>Andy Wood</h2>
-            <hr>
-            <p>Hi, I am Andy Wood. Nice to meet you!</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    quotes = ['Everything before the word ‘but’ is horseshit.',
+              'Some wounds never truly heal, and bleed again at the slightest word.',
+              'The man who passes the sentence should swing the sword.']
+    quote = random.choice(quotes)
+    return render(request, 'about.html', locals())
 
 
 def listing(request):
-    html = '''<!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset='utf-8'>
-            <title>中古機列表</title>
-        </head>
-        <body>
-            <h2>以下是目前本店販售中的產品列表</h2>
-            <hr>
-            <table width=400 border=1 bgcolor='#ccffcc'>
-                {}
-            </table>
-        </body>
-        <body>
-            <h2>以下是目前本店販售中的缺貨列表</h2>
-            <hr>
-            <table width=400 border=1 bgcolor='#ccffcc'>
-                {}
-            </table>
-        </body>
-    </html>
-    '''
     products = Product.objects.all().order_by('price').exclude(qty=0)
-    tags = '<tr><td>品名</td><td>售價</td><td>庫存量</td></tr>'
-    for p in products:
-        tags = tags + '<tr><td>{}</td>'.format(p.name)
-        tags = tags + '<td>{}</td>'.format(p.price)
-        tags = tags + '<td>{}</td></tr>'.format(p.qty)
-
     outStock = Product.objects.all().order_by('price').filter(qty=0)
-    tags2 = '<tr><td>品名</td><td>售價</td><td>庫存量</td></tr>'
-    for p in outStock:
-        tags2 = tags2 + '<tr><td>{}</td>'.format(p.name)
-        tags2 = tags2 + '<td>{}</td>'.format(p.price)
-        tags2 = tags2 + '<td>{}</td></tr>'.format(p.qty)
-
-    return HttpResponse(html.format(tags, tags2))
+    return render(request, 'list.html', locals())
