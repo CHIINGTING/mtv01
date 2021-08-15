cd23ad9fdbbcba3e1d74ff43efa46a791dc927b1
CHIINGTING <tad20077@gmail.com>
2021-08-15 21:16:27 +0800

    add database

--- mysite/models.py
+++ mysite/models.py
@@ -12,3 +12,19 @@ class newTable(models.Model):
     float_f = models.FloatField(null=True)
     int_f = models.IntegerField(default=2010)
     text_f = models.TextField()
+
+
+class Product(models.Model):
+    SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    sku = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+    size = models.CharField(max_length=1, choices=SIZES)
+    qty = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
